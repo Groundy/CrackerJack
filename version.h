d40ec3a537d6276fb4a0a10258db8d0f495eba2b
Groundy <szczygielski107@gmail.com>
2021-06-11 22:24:03 +0200

    Dodanie bibliotek .dll do post buildów w trybie Debug i Release

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 #define VERSION_H
 
-#define VER_FILEVERSION             02,06,2021
-#define VER_FILEVERSION_STR         "02-06-2021/0"
-#define VER_PRODUCTVERSION          02,06,2021
-#define VER_PRODUCTVERSION_STR      "02-06-2021/0"
+#define VER_FILEVERSION             12,06,2021
+#define VER_FILEVERSION_STR         "12-06-2021/0"
+#define VER_PRODUCTVERSION          12,06,2021
+#define VER_PRODUCTVERSION_STR      "12-06-2021/0"
 #define VER_COMPANYNAME_STR         "CrackerJack cop."
 #define VER_FILEDESCRIPTION_STR     "CrackerJack"
 #define VER_INTERNALNAME_STR        "CrackerJack"
