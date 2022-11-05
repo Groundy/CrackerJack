e4034a8772a0f252e47b8ecce3a335b0c5ae9ca9
Groundy <Szczygielski107@gmail.com>
2022-11-05 10:36:19 +0100

    Usunięcie rzeczy związanych z komercjalizacją

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,8 +2,6 @@
 #include "MainMenu.h"
 #include <qtranslator.h>
 #include "Market.h"
-#include "LoginWindow.h"
-QString makeRandomProccessName();
 void test();
 QIcon getIcon();
 void initSettings(QApplication& a);
@@ -16,12 +14,6 @@ int main(int argc, char *argv[])
 	bool ok2 = a.installTranslator(&translator);
 	initSettings(a);
 	test();
-	if(false){
-		LoginWindow w;
-		int result = w.exec();
-		if (result != QDialog::Accepted)
-			return 0;
-	}
 
 	Profile* prof = new Profile();
 	if (true) {
@@ -50,27 +42,6 @@ QIcon getIcon() {
 	QString iconPath = dir.absoluteFilePath("logo.png");
 	return QIcon(iconPath);
 };
-
-QString makeRandomProccessName() {
-	/*This function is designed to make ranom name of Process to avoid giving the same name, which could be easily detected*/
-	const QString possibleCharacters("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ");
-	const int charactersAmount = possibleCharacters.length();
-	const int randomWordsNumber = (qrand() % 3) + 1;
-	QString ProccessName = "";
-	QString wordTmp;
-	for (int i = 0; i < randomWordsNumber; i++){
-		int wordLength = (qrand() % 10) + 1;
-		for (int t = 0; t < wordLength; ++t){
-			int index = qrand() % charactersAmount;
-			QChar nextChar = possibleCharacters.at(index);
-			wordTmp.append(nextChar);
-		}
-		ProccessName.append(wordTmp);
-		ProccessName.append(' ');
-		wordTmp.clear();
-	}
-	return ProccessName;
-}
 
 void initSettings(QApplication &a) {
 	a.setQuitOnLastWindowClosed(true);
