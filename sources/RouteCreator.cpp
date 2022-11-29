edffb97486dfcfedb70f5c233f69c44a763b306f
Groundy <Szczygielski107@gmail.com>
2022-11-29 09:03:03 +0100

    Rewritting Route creator class

--- sources/RouteCreator.cpp
+++ sources/RouteCreator.cpp
@@ -5,78 +5,171 @@ RouteCreator::RouteCreator(QDialog* parent)
 	: QDialog(parent){
 	ui = new Ui::RouteCreator();
 	ui->setupUi(this);
-	/*
-	loadMap(currentChoosenPoint.getFloor());
 	repaintMap();
-	refreshPositionLabel();
+
+	/*
 	ui->movePointUpButton->setEnabled(false);
 	ui->movePointDownButton->setEnabled(false);
 	ui->deletePointButton->setEnabled(false);
 	TRANSLATE_addNamesOfFieldTypesToList();
-	TRANSLATE_gui();
-
-	dirWithRoutes = Utilities::getDirWithCrackerJackTmpFolder(Utilities::FOLDERS_OF_TMP_FOLDER::Routes);
 	*/
 }
+RouteCreator::~RouteCreator() {
+	for each (QImage * map in floorMaps.values()) {
+		delete map;
+	}
+	delete ui;
+}
+void RouteCreator::repaintMap() {
+	int floor = currentPosition.getFloor();
+	if (!loadMap(floor))
+		return;
+
+	const int currFrameSize = ORG_SIZE_OF_FRAME / zoom;
+	QRect frameToDisplay = QRect(
+		currentPosition.getX() - (currFrameSize / 2),
+		currentPosition.getY() - (currFrameSize / 2),
+		currFrameSize,
+		currFrameSize);
+	QPixmap pixMapToShow = getScaledPixMapToPaint(frameToDisplay);
+	ui->imgLabel->setPixmap(pixMapToShow);
+	refreshPositionLabel();
+}
+bool RouteCreator::loadMap(int floor) {
+	try {
+		if (floor < -8 || floor > 7)
+			throw std::exception("Wrong input for loading map function!");
+
+		bool mapAlreadyLoaded = floorMaps.contains(floor);
+		if (!mapAlreadyLoaded) {
+			QString pathToMap = PathResource::getPathToMergedColorMap(floor);
+			QImage* mapImg = new QImage(pathToMap);
+			if (mapImg->isNull())
+				throw std::exception("Map in Route creator could not be read!");
+			floorMaps.insert(floor, mapImg);
+		}
+		return true;
+	}
+	catch (const std::exception& e) {
+		Logger::staticLog(e.what());
+		return false;
+	}
+}
+QPixmap RouteCreator::getScaledPixMapToPaint(QRect frameWithinMap){
+	double ratio = 1.0 * ui->imgLabel->width() / frameWithinMap.width();
+	QImage imgToScale = floorMaps.value(currentPosition.getFloor())->copy(frameWithinMap);
+	QPoint middlePt(imgToScale.width() / 2, imgToScale.height() / 2);
+	imgToScale.setPixel(middlePt, POSITION_MARK_COLOR);
+	QImage displayImg(ui->imgLabel->size(), QImage::Format::Format_ARGB32);
+	for (int x = 0; x < displayImg.width(); x++){
+		int xPixFromSmallImg = x / ratio;
+		for (int y = 0; y < displayImg.height(); y++){
+			int yPixFromSmallImg = y / ratio;
+			uint smallImgPix = imgToScale.pixel(xPixFromSmallImg, yPixFromSmallImg);
+			displayImg.setPixel(x, y, smallImgPix);
+		}
+	}
+	return QPixmap::fromImage(displayImg);
+}
+void RouteCreator::refreshPositionLabel() {
+	/*
+	bool isWalkable = currentPixIsWalkable();
+	QString textBase = tr("   Walkable field: ");
+	QString toAppendYes = tr("Yes");
+	QString toAppendNo = tr("No");
+	QString pointWakabilityStr = isWalkable ? (textBase + toAppendYes) : (textBase + toAppendNo);
+	*/
+
+	QString textToDisplay = currentPosition.toString();
+	ui->positionCordinateLabel->setText(textToDisplay);
+	ui->positionCordinateLabel->repaint();
+}
+void RouteCreator::changePlayerPos(int x, int y) {
+	const auto mapSize = floorMaps.value(currentPosition.getFloor());
+	if (y > 0) {
+		const int maxY = mapSize->height() - 1;
+		int tmpCordinateY = currentPosition.getY() + y;
+		bool ptOutOfRange = tmpCordinateY > maxY;
+		int toSet = ptOutOfRange ? maxY : tmpCordinateY;
+		currentPosition.setY(toSet);
+	}
+	if (y < 0) {
+		int tmpCordinateY = currentPosition.getY() + y;
+		bool ptOutOfRange = tmpCordinateY < 0;
+		int toSet = ptOutOfRange ? 0 : tmpCordinateY;
+		currentPosition.setY(toSet);
+	}
+	if (x > 0) {
+		const int maxX = mapSize->width() - 1;
+		int tmpCordinateX = currentPosition.getX() + x;
+		bool ptOutOfRange = tmpCordinateX > maxX;
+		int toSet = ptOutOfRange ? maxX : tmpCordinateX;
+		currentPosition.setX(toSet);
+	}
+	if (x < 0) {
+		int tmpCordinateX = currentPosition.getX() + x;
+		bool ptOutOfRange = tmpCordinateX < 0;
+		int toSet = ptOutOfRange ? 0 : tmpCordinateX;
+		currentPosition.setX(toSet);
+	}
+};
+
+//slots
+void RouteCreator::zoomChanged() {
+	zoom = (2 * zoom == 8) ? 1 : (2 * zoom);
+	QString textOnButton = QString("Zoom %1x").arg(QString::number(zoom));
+	ui->zoomButton->setText(textOnButton);
+	repaintMap();
+}
+void RouteCreator::cancelButtonPressed(){
+	this->reject();
+}
+void RouteCreator::mapMoved() {
+	if (sender() == ui->moveMapDown_1)
+		changePlayerPos(0, 1);
+	if (sender() == ui->moveMapDown_2)
+		changePlayerPos(0, 5);
+	if (sender() == ui->moveMapDown_3)
+		changePlayerPos(0, 100);
+	if (sender() == ui->moveMapLeft_1)
+		changePlayerPos(-1, 0);
+	if (sender() == ui->moveMapLeft_2)
+		changePlayerPos(-5, 0);
+	if (sender() == ui->moveMapLeft_3)
+		changePlayerPos(-100, 0);
+	if (sender() == ui->moveMapUp_1)
+		changePlayerPos(0, -1);
+	if (sender() == ui->moveMapUp_2)
+		changePlayerPos(0, -5);
+	if (sender() == ui->moveMapUp_3)
+		changePlayerPos(0, -100);
+	if (sender() == ui->moveMapRight_1)
+		changePlayerPos(1, 0);
+	if (sender() == ui->moveMapRight_2)
+		changePlayerPos(5, 0);
+	if (sender() == ui->moveMapRight_3)
+		changePlayerPos(100, 0);
+
+	repaintMap();
+}
+void RouteCreator::floorChanged(){
+	int currentF = currentPosition.getFloor();
+	if (sender() == ui->upFloor) {
+		if (currentF == 7)
+			return;
+		currentPosition.setFloor(currentF +1);
+
+	}
+	else if (sender() == ui->lowerFloor) {
+		if (currentF == -8)
+			return;
+		currentPosition.setFloor(currentF - 1);
+	}
+	repaintMap();
+}
+
+
 /*
-
-void RouteCreator::imgMoved_left(){
-	moveMap(DIRECTIONS::LEFT, 1);
-}
-void RouteCreator::imgMoved_left_fast(){
-	moveMap(DIRECTIONS::LEFT, 5);
-}
-void RouteCreator::imgMoved_left_very_fast(){
-	moveMap(DIRECTIONS::LEFT, 100);
-}
-void RouteCreator::imgMoved_right(){
-	moveMap(DIRECTIONS::RIGHT, 1);
-}
-void RouteCreator::imgMoved_right_fast(){
-	moveMap(DIRECTIONS::RIGHT, 5);
-}
-void RouteCreator::imgMoved_right_very_fast(){
-	moveMap(DIRECTIONS::RIGHT, 100);
-}
-void RouteCreator::imgMoved_top(){
-	moveMap(DIRECTIONS::UP, 1);
-}
-void RouteCreator::imgMoved_top_fast() {
-	moveMap(DIRECTIONS::UP, 5);
-}
-void RouteCreator::imgMoved_top_very_fast() {
-	moveMap(DIRECTIONS::UP, 100);
-}
-void RouteCreator::imgMoved_down() {
-	moveMap(DIRECTIONS::DOWN, 1);
-}
-void RouteCreator::imgMoved_down_fast() {
-	moveMap(DIRECTIONS::DOWN, 5);
-}
-void RouteCreator::imgMoved_down_very_fast() {
-	moveMap(DIRECTIONS::DOWN, 100);
-}
-
-
-void RouteCreator::floorDown(){
-	int currentF = currentChoosenPoint.getFloor();
-	if (currentF == -8)
-		return;
-	currentChoosenPoint.setFloor(currentF - 1);
-	loadMap(currentChoosenPoint.getFloor());
-	refreshPositionLabel();
-	repaintMap();
-}
-
-void RouteCreator::floorUp(){
-	int currentF = currentChoosenPoint.getFloor();
-	if (currentF == 7)
-		return;
-	currentChoosenPoint.setFloor(currentF + 1);
-	loadMap(currentChoosenPoint.getFloor());
-	refreshPositionLabel();
-	repaintMap();
-}
 
 void RouteCreator::addPoint(){
 	QListWidget *list = ui->pointsList;
@@ -128,42 +221,6 @@ void RouteCreator::deletePoint(){
 	list->setCurrentRow(rowToDelete);
 }
 
-bool RouteCreator::loadMap(int floor){
-	//floor (-8 bottom, 0 ground, 7 top)
-	floor += 8;// (0 bottom, 8 ground, 15 top)
-	floor = 15 - floor;// (15 bottom, 7 ground, 0 top)
-	bool wrongInput = floor < 0 || floor >15;
-	if (wrongInput) {
-		//Logger::logPotenialBug("Wrong input, input=" + QString::number(floor), "RouteCreator", "loadMap");
-	}
-
-	QString floorAsStr = QString::number(floor);
-	if (floorAsStr.size() == 1)
-		floorAsStr.push_front(QString::number(0));
-		
-
-	QString mapFileName = "floor-" + floorAsStr + "-map.png";
-	QString walkMapFileName = "floor-" + floorAsStr + "-path.png";
-	QString pathToMap = dirWithMaps.absoluteFilePath(mapFileName);
-	QString pathToWalkMap = dirWithMaps.absoluteFilePath(mapFileName);
-
-	QImage mapTmpImg, walkMapTmpImg;
-	bool mapLoadedCorrectly = mapTmpImg.load(pathToMap);
-	bool walkMapLoadedCorrectly = walkMapTmpImg.load(pathToWalkMap);
-
-	if (mapLoadedCorrectly && walkMapLoadedCorrectly) {
-		currentMap = mapTmpImg;
-		currentMapOfWalkability = walkMapTmpImg;
-	}
-	else {
-		QString text = QString("Can't load file \n %1 \n or \n %2").arg(pathToMap, pathToWalkMap);
-		//Logger::logPotenialBug(text, "RouteCreator", "loadMap");
-		return false;
-	}
-
-	return true;
-}
-
 void RouteCreator::repaintList(){
 	QListWidget* list = ui->pointsList;
 	list->clear();
@@ -172,31 +229,6 @@ void RouteCreator::repaintList(){
 		list->addItem(var);
 	}
 	list->repaint();
-}
-
-QPixmap RouteCreator::getPixMapWithZoomAndCenterPix(QImage imgWithMap, QSize sizeToScale) {
-	QPixmap pixMaptoDisplay(imgWithMap.size());
-	double ratio = 1.0 / (2*zoom);
-	int xStart = imgWithMap.width() * (0.5 - ratio);
-	int xEnd = imgWithMap.width() * (0.5 + ratio);
-	int yStart = imgWithMap.height() * (0.5 - ratio);
-	int yEnd = imgWithMap.height() * (0.5 + ratio);
-	QImage displayImg(sizeToDisplay, QImage::Format::Format_ARGB32);
-
-	for (size_t x = xStart; x < xEnd; x++) {
-		for (size_t y = yStart; y < yEnd; y++) {
-			bool itsMiddlePix = (x == imgWithMap.width() / 2) && (y == imgWithMap.height() / 2);
-			uint pixColToSet = itsMiddlePix ? CHOOSEN_POINT_COLOR : imgWithMap.pixel(x, y);
-			int startPoint_X = zoom * (x - xStart);
-			int startPoint_Y = zoom * (y - yStart);
-			for (size_t xTMP = 0; xTMP < zoom; xTMP++) {
-				for (size_t yTMP = 0; yTMP < zoom; yTMP++)
-					displayImg.setPixel(startPoint_X + xTMP, startPoint_Y + yTMP, pixColToSet);
-			}
-		}
-	}
-	pixMaptoDisplay = QPixmap::fromImage(displayImg).scaled(sizeToScale);
-	return pixMaptoDisplay;
 }
 
 void RouteCreator::TRANSLATE_addNamesOfFieldTypesToList(){
@@ -226,125 +258,6 @@ void RouteCreator::TRANSLATE_addNamesOfFieldTypesToList(){
 	box->repaint();
 }
 
-void RouteCreator::TRANSLATE_gui(){
-	QString left = tr("left");
-	ui->moveMapLeft_1->setText(left);
-	ui->moveMapLeft_2->setText(left + " 5x");
-	ui->moveMapLeft_3->setText(left + " 100x");
-	ui->moveMapLeft_1->repaint();
-	ui->moveMapLeft_2->repaint();
-	ui->moveMapLeft_3->repaint();
-
-	QString up = tr("up");
-	ui->moveMapUp_1->setText(up);
-	ui->moveMapUp_2->setText(up + " 5x");
-	ui->moveMapUp_3->setText(up + " 100x");
-	ui->moveMapUp_1->repaint();
-	ui->moveMapUp_2->repaint();
-	ui->moveMapUp_3->repaint();
-
-	QString down = tr("down");
-	ui->moveMapDown_1->setText(down);
-	ui->moveMapDown_2->setText(down + " 5x");
-	ui->moveMapDown_3->setText(down + " 100x");
-	ui->moveMapDown_1->repaint();
-	ui->moveMapDown_2->repaint();
-	ui->moveMapDown_3->repaint();
-
-	QString right = tr("right");
-	ui->moveMapRight_1->setText(right);
-	ui->moveMapRight_2->setText(right + " 5x");
-	ui->moveMapRight_3->setText(right + " 100x");
-	ui->moveMapRight_1->repaint();
-	ui->moveMapRight_2->repaint();
-	ui->moveMapRight_3->repaint();
-
-	QString floorDown = tr("floor down");
-	ui->lowerFloor->setText(floorDown);
-	ui->lowerFloor->repaint();
-
-	QString floorUp = tr("floor up");
-	ui->upFloor->setText(floorUp);
-	ui->upFloor->repaint();
-
-	QString pointUp = tr("move up");
-	ui->movePointUpButton->setText(pointUp);
-	ui->movePointUpButton->repaint();
-
-	QString pointDown = tr("move down");
-	ui->movePointDownButton->setText(pointDown);
-	ui->movePointDownButton->repaint();
-
-	QString deletePoint = tr("delete point");
-	ui->deletePointButton->setText(deletePoint);
-	ui->deletePointButton->repaint();
-
-	QString addPoint = tr("add point");
-	ui->addPointButton->setText(addPoint);
-	ui->addPointButton->repaint();
-
-	QString cancelButton = tr("Cancel");
-	ui->cancelButton->setText(cancelButton);
-	ui->cancelButton->repaint();
-
-	QString finishButton = tr("Finish");
-	ui->finishButton->setText(finishButton);
-	ui->finishButton->repaint();
-
-	QString loadButton = tr("Load route");
-	ui->loadRouteButton->setText(loadButton);
-	ui->loadRouteButton->repaint();
-
-	QString helpButton = tr("Help");
-	ui->helpButton->setText(helpButton);
-	ui->helpButton->repaint();
-
-	QString checkRouteButton = tr("Check route correctness");
-	ui->checkButton->setText(checkRouteButton);
-	ui->checkButton->repaint();
-
-}
-
-bool RouteCreator::repaintMap(){
-	const QSize sizeOfLabel = ui->imgLabel->size();
-	const int WIDTH = currentMap.width();
-	const int HEIGHT = currentMap.height();
-
-	int startX = currentChoosenPoint.getX() - sizeToDisplay.width() / 2;
-	int width =  sizeToDisplay.width();
-	int startY = currentChoosenPoint.getY() - sizeToDisplay.height() / 2;
-	int height =  sizeToDisplay.height();
-
-
-	QRect rect(startX, startY, width, height);
-	QImage orgImg = currentMap.copy(rect);
-	QPixmap pixMaptoDisplay;
-	pixMaptoDisplay = getPixMapWithZoomAndCenterPix(orgImg, sizeOfLabel);
-	ui->imgLabel->setPixmap(pixMaptoDisplay);
-	ui->imgLabel->repaint();
-	return true;
-}
-
-void RouteCreator::zoomChanged(){ 
-	zoom = (2 * zoom == 8) ? 1 : (2 * zoom);
-	QString textOnButton = QString("Zoom %1x").arg(QString::number(zoom));
-	ui->zoomButton->setText(textOnButton);
-	repaintMap();
-}
-
-void RouteCreator::refreshPositionLabel(){
-	bool isWalkable = currentPixIsWalkable();
-	QString textBase = tr("   Walkable field: ");
-	QString toAppendYes = tr("Yes");
-	QString toAppendNo = tr("No");
-	QString pointWakabilityStr = isWalkable ? (textBase + toAppendYes) : (textBase + toAppendNo);
-
-
-	QString textToDisplay = currentChoosenPoint.toString() + pointWakabilityStr;
-	ui->positionCordinateLabel->setText(textToDisplay);
-	ui->positionCordinateLabel->repaint();
-}
-
 void RouteCreator::selectedItemOnListChanged(){
 	QListWidget* list = ui->pointsList;
 	int size = route.size();
@@ -364,9 +277,7 @@ void RouteCreator::selectedItemOnListChanged(){
 	ui->deletePointButton->setEnabled(desiredState);
 }
 
-void RouteCreator::cancelButtonPressed(){
-	this->reject();
-}
+
 
 void RouteCreator::finishButtonPressed(){
 	bool routeOk = checkRouteButtonPressed();
--- sources/RouteCreator.h
+++ sources/RouteCreator.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <qdialog.h>
-#include "MinimapAnalyzer.h"
 #include <qsize.h>
+//#include <qfiledialog.h>
+#include <qpixmap.h>
+#include <qimage.h>
+
 #include "Point3D.h"
 #include "Route.h"
-#include <qfiledialog.h>
 #include "RGBstruct.h"
-#include "Spell.h"
+//#include "Spell.h"
 
 namespace Ui { class RouteCreator; };
 
@@ -15,40 +17,39 @@ class RouteCreator : public QDialog {
 	Q_OBJECT
 public:
 	RouteCreator(QDialog* parent = Q_NULLPTR);
-	~RouteCreator() {
-		delete ui;
-	}
+	~RouteCreator();
+
+public slots:
+	void mapMoved();
+	void floorChanged();
+	void zoomChanged();
+	void cancelButtonPressed();
 private:
 	Ui::RouteCreator* ui;
+	QMap<int, QImage*> floorMaps = {};
+	Point3D currentPosition = Point3D(625, 1265, 0);//thais temple
+	const int ORG_SIZE_OF_FRAME = 112;
+	const uint POSITION_MARK_COLOR = qRgb(0, 255, 255);
+	int zoom = 1;
+
+	void repaintMap();
+	bool loadMap(int floor);
+	QPixmap getScaledPixMapToPaint(QRect frameWithinMap);
+	void changePlayerPos(int x, int y);
+	void refreshPositionLabel();
 };
 
 /*
 public:
 
-	~RouteCreator();
 public slots:
-	void imgMoved_left();
-	void imgMoved_left_fast();
-	void imgMoved_left_very_fast();
-	void imgMoved_right();
-	void imgMoved_right_fast();
-	void imgMoved_right_very_fast();
-	void imgMoved_top();
-	void imgMoved_top_fast();
-	void imgMoved_top_very_fast();
-	void imgMoved_down();
-	void imgMoved_down_fast();
-	void imgMoved_down_very_fast();
-
-	void floorDown();
-	void floorUp();
+	~RouteCreator();
 	void addPoint();
 	void moveListItemUp();
 	void moveListItemDown();
 	void deletePoint();
-	void zoomChanged();
 	void selectedItemOnListChanged();
-	void cancelButtonPressed();
+
 	void finishButtonPressed();
 	void loadRouteButtonPressed();
 	void helpButtonPressed();
@@ -56,27 +57,11 @@ public slots:
 	bool currentPixIsWalkable();
 
 private:
-	enum class DIRECTIONS { UP, DOWN, LEFT, RIGHT };
-
-	QImage currentMap;
-	QImage currentMapOfWalkability;
 	Route route;
 	QDir dirWithRoutes;
-	QDir dirWithMaps = QDir("C:\\Users\\ADMIN\\Desktop\\maps");//todo zmienic
-	int zoom = 1;
-	Point3D currentChoosenPoint = { 625,1265,0};
-	const QSize sizeToDisplay = { 108,108 };
-	const uint CHOOSEN_POINT_COLOR = qRgb(60,60,60);
 
-	bool loadMap(int floor);
-	void repaintList();
-	bool repaintMap();
-	void refreshPositionLabel();
-	void moveMap(DIRECTIONS direction, int step);
 	QPixmap getPixMapWithZoomAndCenterPix(QImage imgWithMap, QSize sizeToScale);
 	Route::FIELDS_TYPE getFieldTypeComboBox();
 	void TRANSLATE_addNamesOfFieldTypesToList();
-	void TRANSLATE_gui();
-
 };
 */
